c7eb4a2ddaf2d182cadf6aa3bdee65140a7771d4
modernator <rico345100@gmail.com>
2018-09-22 19:18:18 +0900

    TaskPanel 퍼블리싱, ListPanel의 Overflow 스크롤 영역 변경

--- src/js/components/TaskPanel/index.js
+++ src/js/components/TaskPanel/index.js
@@ -6,7 +6,30 @@ class TaskPanel extends Component<{}, {}> {
 	render() {
 		return (
 			<div className={styles.taskPanel}>
-				<h1>Task Panel</h1>
+				<div className={styles.taskPanelTop}>
+					<div className={styles.taskHeader}>A List</div>
+					<button className={styles.taskAddButton}>{'+'}</button>
+				</div>
+
+				<div className={styles.taskContainer}>
+					<div className={styles.taskItem}>
+						<div className={styles.taskItemCheckboxContainer}>
+							<input className={styles.taskItemCheckbox} type="checkbox" />
+							<span class={[styles.taskItemCheckMark, styles.taskItemCheckBoxChecked].join(' ')}></span>
+						</div>
+
+						<div className={styles.taskItemLabel}>Korea</div>
+					</div>
+
+					<div className={styles.taskItem}>
+						<div className={styles.taskItemCheckboxContainer}>
+							<input className={styles.taskItemCheckbox} type="checkbox" />
+							<span class={styles.taskItemCheckMark}></span>
+						</div>
+
+						<div className={styles.taskItemLabel}>Korea</div>
+					</div>
+				</div>
 			</div>
 		);
 	}
