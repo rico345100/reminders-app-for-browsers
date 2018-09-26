6c4b3bee06e1f48b8b2401083db3c2339fc75748
Hanju Kim <hanjukim@gmail.com>
2018-09-26 16:35:51 +0900

    build does build only.

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,111 +5,116 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+const plugins = [
+	new CleanWebpackPlugin(['dist']),
+	new HtmlWebpackPlugin({
+		template: './src/index.html'
+	}),
+	new MiniCssExtractPlugin({
+		filename: '[name].css',
+		chunkFilename: '[id].css'
+	}),
+	new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en/)
+];
+
+if (process.env.BUILD_ANALYZE) {
+	plugins.push(
+		new (require('webpack-bundle-analyzer')).BundleAnalyzerPlugin()
+	);
+}
 
 module.exports = {
-    entry: {
-        bundle: './src/js/index.js',
-        vendor: [
-            'react', 
-            'react-dom', 
-            'react-redux',
-            'redux',
-            'prop-types', 
-            'babel-polyfill',
-        ]
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/',
-        filename: '[name].[hash].js'
-    },
-    optimization: {
-        splitChunks: {
-            cacheGroups: {
-                default: false,
-                commons: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: "vendor",
-                    chunks: "all"
-                }
-            }
-        }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: 'html-loader',
-                        options: {
-                            minimize: true
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader, 
-                    {
-                        loader: "css-loader",
-                        options: {
-                            modules: true
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    {
-                        loader: 'style-loader'
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            localIdentName: '[name].[sha512:hash:base32].[local]',
-                            modules: true
-                        }
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            includePaths: [
-                                './src/css'
-                            ],
-                        }
-                    }
-                ]
-            }
-        ]
-    },
-    plugins: [
-      new CleanWebpackPlugin(['dist']),
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-            chunkFilename: '[id].css'
-        }),
-        new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en/),
-        new BundleAnalyzerPlugin()
-    ],
-    resolve: {
-        modules: [
-            path.resolve('./src'),
-            path.resolve('./src/js'),
-            path.resolve('./node_modules')
-        ]
-    }
+	entry: {
+		bundle: './src/js/index.js',
+		vendor: [
+			'react',
+			'react-dom',
+			'react-redux',
+			'redux',
+			'prop-types',
+			'babel-polyfill'
+		]
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		publicPath: '/',
+		filename: '[name].[hash].js'
+	},
+	optimization: {
+		splitChunks: {
+			cacheGroups: {
+				default: false,
+				commons: {
+					test: /[\\/]node_modules[\\/]/,
+					name: 'vendor',
+					chunks: 'all'
+				}
+			}
+		}
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			},
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: 'html-loader',
+						options: {
+							minimize: true
+						}
+					}
+				]
+			},
+			{
+				test: /\.css$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+						options: {
+							modules: true
+						}
+					}
+				]
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					{
+						loader: 'style-loader'
+					},
+					{
+						loader: 'css-loader',
+						options: {
+							localIdentName:
+								'[name].[sha512:hash:base32].[local]',
+							modules: true
+						}
+					},
+					{
+						loader: 'sass-loader',
+						options: {
+							includePaths: ['./src/css']
+						}
+					}
+				]
+			}
+		]
+	},
+	plugins,
+	resolve: {
+		modules: [
+			path.resolve('./src'),
+			path.resolve('./src/js'),
+			path.resolve('./node_modules')
+		]
+	}
 };
